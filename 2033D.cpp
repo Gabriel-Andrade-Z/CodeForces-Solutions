08631629a17bd5f0ad05d371cff52091f37f0a24
Gabriel-Andrade-Z <gabrielasantana1602@gmail.com>
2025-01-14 10:54:28 -0300

    Add

--- 2033D.cpp
+++ 2033D.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
+typedef long long ll;
 using namespace std;
 
 void solve() {
-
+	int n, cnt = 0; ll curr = 0; cin >> n;
+    	vector<int> a(n);
+    	for (int& i : a) cin >> i;
+    	set<ll> sums; sums.insert(0);
+    	for (int i = 0; i < n; ++i) {
+        	curr += a[i];
+        	if (sums.count(curr)) { ++cnt; sums.clear(); sums.insert(0); curr = 0; }
+        	sums.insert(curr);
+    	}
+    	cout << cnt << '\n';
 }
 
 int main() {
