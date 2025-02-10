2968c366ac691e880e183c741af7eba20b685f19
Gabriel-Andrade-Z <gabrielasantana1602@gmail.com>
2025-02-10 07:04:11 -0300

    Add

---
+++ 1829G.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+}
+
+int main() {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(0);
+	int t; cin >> t;
+	while (t--) solve();
+	return 0;
+}
